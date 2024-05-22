7d324a62b8cc77e3ac1cd0948086ad1499ffea10
Nathan Soucier <soucienp@gmail.com>
2024-05-22 12:10:15 -0400

    adding password for db

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 /** Database for lunchly */
 
 const pg = require("pg");
+const password = require('./secrets'); // importing password from hidden file
 
-const db = new pg.Client("postgresql:///lunchly");
+const db = new pg.Client(`postgresql://soucier:${password}@127.0.0.1:5432/lunchly`);
 
 db.connect();
 
